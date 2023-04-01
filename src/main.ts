50a50584ca4e35a87ec0a359b13c0851dea11a93
RovarQRoom <rovar.07000120@outlook.com>
2023-04-01 09:33:05 +0300

    Microservice Sample Created For Todo List Project

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'tasks_queue',
+      prefetchCount: 1,
+      noAck: false,
+      queueOptions: {
+        name: 'tasks_queue',
+        type: 'direct',
+        durable: true,
+      },
+    },
+  });
+  await app.listen();
+}
+bootstrap();
